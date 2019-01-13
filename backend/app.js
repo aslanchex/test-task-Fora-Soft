0db228bbea7b7a494d94960e1ecc27dd5a862f99
aslanchex <aslan.kashiev@mail.ru>
2019-01-13 13:02:22 +0300

    Installed socket.io on client - side & set up socket listening to the running server port

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const socket = require('socket.io')
+const express = require("express");
+const socket = require("socket.io");
 const app = express();
 
-
-
-server = app.listen(8080, function(){
-    console.log('server is running on port 8080')
+let server = app.listen(8080, function() {
+  console.log("server is running on port 8080");
 });
 
-io = socket(server);
-io.on('connection', (socket) => {
-    console.log(socket.id);
+let io = socket(server);
+io.on("connection", socket => {
+  console.log(socket.id);
 });
--- src/Chat.js
+++ src/Chat.js
@@ -1,47 +1,61 @@
-import React from 'react';
+import React from "react";
+import io from "socket.io-client";
 
-class Chat extends React.Component{
-    constructor(props){
-        super(props);
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            username: '',
-            message: '',
-            messages: []
-        };
-    }
-    
-    render(){
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-8">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="card-title">Chat room</div>
-                                <hr/>
-                                <div className="messages">
-                                {/* loop through all the messages which we will have and display author’s name and his message */}
-                                    {this.state.messages.map(message => {
-                                        return (
-                                            <div>{message.author}: {message.message}</div>
-                                        )
-                                    })}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <input type="text" placeholder="Username" className="form-control"/>
-                            <br/>
-                            <input type="text" placeholder="Message" className="form-control"/>
-                            <br/>
-                            <button className="btn btn-primary form-control">Send</button>
-                        </div>
-                    </div>
+    this.state = {
+      username: "",
+      message: "",
+      messages: []
+    };
+
+    // socket listening to the running server port
+    this.socket = io("localhost:8080");
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-8">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">Chat room</div>
+                <hr />
+                <div className="messages">
+                  {/* loop through all the messages which we will have and display author’s name and his message */}
+                  {this.state.messages.map(message => {
+                    return (
+                      <div>
+                        {message.author}: {message.message}
+                      </div>
+                    );
+                  })}
                 </div>
+              </div>
             </div>
-        )
-    }
+            <div className="card-footer">
+              <input
+                type="text"
+                placeholder="Username"
+                className="form-control"
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="Message"
+                className="form-control"
+              />
+              <br />
+              <button className="btn btn-primary form-control">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Chat;
